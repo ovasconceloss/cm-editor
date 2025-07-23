81dd19f8c5c3f14dc00d31fe229e78cac08a7e74
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-23 21:24:09 +0100

    feat: define the application state in lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,21 @@
 pub mod app;
 pub mod core;
+pub mod state;
 pub mod use_cases;
 
-use crate::app::commands::{file::commands::new_file, system::commands::exit_app};
+use std::sync::Mutex;
+use crate::{app::commands::{file::commands::{new_file, set_file}, system::commands::exit_app}, state::DatabaseState};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(Mutex::new(DatabaseState { 
+            database_path: String::new() 
+        }))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             new_file,
+            set_file,
             exit_app,
         ])
         .run(tauri::generate_context!())
