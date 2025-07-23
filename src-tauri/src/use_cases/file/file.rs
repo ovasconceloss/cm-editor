98e66c6debcdae37331784559215aa76c3e0d7a0
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-23 21:24:38 +0100

    feat: create a function to define the database_path of state.rs

--- src-tauri/src/use_cases/file/file.rs
+++ src-tauri/src/use_cases/file/file.rs
@@ -1,10 +1,17 @@
+use tauri::State;
+use crate::state::SharedDatabase;
 use std::{fs::File, path::PathBuf};
 
 pub fn new(name: String) -> Result<PathBuf, ()> {
   let file_name: String = format!("{}.db", name);
   let file_path: PathBuf = PathBuf::from(&file_name);
 
-  let _file = File::create(&file_path).expect("ERROR");
+  let _file = File::create(&file_path).unwrap();
 
   Ok(file_path)
+}
+
+pub fn set(database_path: String, state: State<'_, SharedDatabase>) {
+  let mut database = state.lock().unwrap();
+  database.database_path = database_path;
 }
