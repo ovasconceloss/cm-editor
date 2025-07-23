0d67f02d02f34908d527e9454bf7c86dbc641bac
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-23 15:45:02 +0100

    feat: create the home screen (base)

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,11 @@
+import Menu from "./components/menubar";
+
+function Home() {
+  return (
+    <div className="w-screen h-screen select-none">
+      <Menu />
+    </div>
+  );
+};
+
+export default Home;
